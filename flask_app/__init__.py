bfc1cd923b9ecd358556923099d5512b2a13fd7c
sdress <sarah.dresser1@gmail.com>
2022-03-15 03:36:55 -0700

    updating base files

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = "some key" # create secret key
+app.secret_key = "mysupersecretkey" # create secret key
 
 # don't forget to import flask, pymysql, and flask-bcrypt in shell
